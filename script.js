d45673246697fa2f0296ccfc1e194900372134c7
pmunoz36 <114035920+pmunoz36@users.noreply.github.com>
2025-09-01 21:25:13 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Script básico para animar el scroll suave al hacer clic en los enlaces
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
