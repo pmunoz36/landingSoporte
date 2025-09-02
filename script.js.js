122796d24abb909a6778a73afc6c6b6aa26fb75b
pmunoz36 <114035920+pmunoz36@users.noreply.github.com>
2025-09-01 21:24:36 -0500

    Delete script.js.js

--- script.js.js
+++
@@ -1,9 +0,0 @@
-// Script básico para animar el scroll suave al hacer clic en los enlaces
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth"
-    });
-  });
-});
